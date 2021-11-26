5e2fc3b765a9439d19f84fcabd534ccb70d5a1e0
sarim-itsol-2020 <36129452+muhammadsarim555@users.noreply.github.com>
2021-11-26 18:31:48 +0500

    add work

--- pages/test/index.js
+++ pages/test/index.js
@@ -1,33 +1,27 @@
 import styles from "../../styles/Test.module.css";
 
+import Link from "next/Link";
+
 export const getStaticProps = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await res.json();
-
-  console.log("func")
-
 
   return {
     props: { ninjas: data },
   };
 };
 
-
-    console.log("out")
-
 const Ninjas = ({ ninjas }) => {
-  
-    console.log("in")
-  
-    return (
+  //
+  return (
     <div>
       <h1>All Ninjas</h1>
       {ninjas.map((ninja) => (
-        <div key={ninja.id}>
+        <Link href={`/test/${ninja.id}`} key={ninja.id}>
           <a className={styles.single}>
             <h3>{ninja.name}</h3>
           </a>
-        </div>
+        </Link>
       ))}
     </div>
   );
